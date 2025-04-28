103c32a229700e64522a44350e43e07e0d5df67b
alexia <vieiralexiac@gmail.com>
2025-04-28 19:15:16 -0300

    adicionando modificações

--- app.py
+++ app.py
@@ -1213,6 +1213,9 @@ def mudar_senha():
 
 if __name__ == "__main__":
     with app.app_context():
-        db.create_all()
+        try:
+            db.create_all()
+        except Exception as e:
+            print(f"Erro ao criar tabelas: {e}")
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host="0.0.0.0", port=port)
