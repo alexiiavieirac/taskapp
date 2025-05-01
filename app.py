dabf2cbcb9050e2fa69da8a27c0d433f33e3b6ce
alexia <vieiralexiac@gmail.com>
2025-05-01 12:21:52 -0300

    modificação no app

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import sys
 from pathlib import Path
 from dotenv import load_dotenv
 from app import create_app
-from extensions import socketio
+from app.extensions.socketio import socketio
 
 sys.path.insert(0, str(Path(__file__).parent))
 
@@ -12,5 +12,8 @@ load_dotenv()
 app = create_app()
 
 if __name__ == "__main__":
-    socketio.run(app, debug=os.getenv('FLASK_DEBUG', 'false').lower() == 'true', 
-                 port=int(os.getenv('FLASK_PORT', 5000)), use_reloader=False)
+    socketio = app.extensions.get('socketio')
+    if socketio:
+        socketio.run(app, debug=os.getenv('FLASK_DEBUG', 'false').lower() == 'true', port=int(os.getenv('FLASK_PORT', 5000)))
+    else:
+        app.run(debug=os.getenv('FLASK_DEBUG', 'false').lower() == 'true', port=int(os.getenv('FLASK_PORT', 5000)))
