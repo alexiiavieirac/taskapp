89a97fa0a5ff900e2dee47aaf91546e55b663080
alexia <vieiralexiac@gmail.com>
2025-05-02 16:47:17 -0300

    nova tentativa para fazer funcionar no railway

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ if __name__ == "__main__":
     debug_mode = os.getenv('FLASK_DEBUG', 'false').lower() == 'true'
     port = int(os.getenv('PORT', 5000))
 
-    app.run(debug=debug_mode, host="0.0.0.0", port=port)
+    socketio.run(app, debug=debug_mode, host="0.0.0.0", port=port)
