d175216e2f2b53b50df72127df3fc527516ff6d3
alexia <vieiralexiac@gmail.com>
2025-05-05 12:40:58 -0300

    atualizando var

--- app/extensions/serializer.py
+++ app/extensions/serializer.py
@@ -1,19 +1,19 @@
 from itsdangerous import URLSafeTimedSerializer
 
-serializer = None
+s = None
 
 def init_serializer(app):
-    global serializer
-    serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+    global s
+    s = URLSafeTimedSerializer(app.config['SECRET_KEY'])
 
 
 def generate_token(email):
-    return serializer.dumps(email, salt='email-confirm-salt')
+    return s.dumps(email, salt='email-confirm-salt')
 
 
 def confirm_token(token, expiration=3600):
     try:
-        email = serializer.loads(token, salt='email-confirm-salt', max_age=expiration)
+        email = s.loads(token, salt='email-confirm-salt', max_age=expiration)
     except Exception:
         return None
     return email
