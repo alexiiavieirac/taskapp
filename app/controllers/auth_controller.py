2571050e40ca05d60080e28ea04baa3a576772a8
alexia <vieiralexiac@gmail.com>
2025-05-06 11:45:42 -0300

    importando biblioteca

--- app/controllers/auth_controller.py
+++ app/controllers/auth_controller.py
@@ -3,6 +3,7 @@ from flask_login import login_user, logout_user, login_required
 from flask_mail import Message
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
+import app
 from app.extensions import mail
 from app.extensions.serializer import generate_token, confirm_token
 from app.utils.network_utils import is_safe_url
