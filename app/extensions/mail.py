71cb7bc98ddece00a68d372ab383a26c49176649
alexia <vieiralexiac@gmail.com>
2025-05-06 12:04:24 -0300

    mudando porta

--- app/extensions/mail.py
+++ app/extensions/mail.py
@@ -9,8 +9,8 @@ def init_mail(app):
     mail.init_app(app)
     app.config['MAIL_SERVER'] = 'smtp.gmail.com'
     app.config['MAIL_PORT'] = int(os.getenv("MAIL_PORT"))
-    app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS', False) 
-    app.config['MAIL_USE_SSL'] = os.getenv('MAIL_USE_SSL', True) 
+    app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS', True) 
+    app.config['MAIL_USE_SSL'] = os.getenv('MAIL_USE_SSL', False) 
     app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
     app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD")
     app.config['MAIL_DEFAULT_SENDER'] = os.getenv("MAIL_DEFAULT_SENDER")
