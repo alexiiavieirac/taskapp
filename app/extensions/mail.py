773fe9b75d32ba5e71709963f64636fd7131cd7a
alexia <vieiralexiac@gmail.com>
2025-06-09 19:23:00 -0300

    mudança

--- app/extensions/mail.py
+++ app/extensions/mail.py
@@ -6,7 +6,7 @@ mail = Mail()
 def init_mail(app):
     mail.init_app(app)
     app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-    app.config['MAIL_PORT'] = os.getenv("MAIL_PORT")
+    app.config['MAIL_PORT'] = int(os.getenv("MAIL_PORT"))
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
     app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD")
